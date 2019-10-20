eff318790fad30918e17d6d2e4da7aa20976789b
Fernando Gonzalez <gonzalez0fer@gmail.com>
2019-10-19 22:15:12 -0400

    ready to write rules when the requirements comes out

---
+++ Etapa2-Parser/parser.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#######################################
+# CI3715 Traductores e Interpretadores
+# Entrega 2. 
+# Fernando Gonzalez 08-10464
+# Kevin Mena 13-10869
+#######################################
+
+
+
+
+
+# Variable global del primer error del parser si existe.
+global parser_error
+parser_error = False
