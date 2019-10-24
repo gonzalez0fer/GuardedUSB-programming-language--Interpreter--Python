252950d26498ad052cd681377a23d6d2cad99b2a
Fernando Gonzalez <gonzalez0fer@gmail.com>
2019-10-23 23:45:30 -0400

    Rule Writing inProgress, NOTE: AbsSyntaxTree print methods NOT are the right ones.

--- Etapa2-Parser/g_parser.py
+++ Etapa2-Parser/g_parser.py
@@ -10,24 +10,29 @@ import ply.yacc as yacc
 from g_lexer import *
 from g_AbsSyntaxTree import *
 
+# De aqui en adelante escribimos las reglas que definiran la gramatica
+# de nuestro lenguaje para filtrar errores sintacticos y construir 
+# nuestro arbol sintactico abstracto.
 
+def p_expressions(p):
+	'''expression : expression TkPlus expression
+				| expression TkMinus expression
+				| expression TkMult expression
+				| expression TkDiv expression
+				| expression TkMod expression
+                | TkOpenPar expression TkClosePar 
+                | TkOBracket expression TkCBracket
+    '''
+	if len(p) == 4 :
+		# Definicion de la regla para Operadores aritmeticos binarios.
+		if (p[2]=='+' or p[2]=='-' or p[2]=='*' or p[2]=='/' or p[2]=='%') :
+			p[0] = BinaryOperator(p[1],p[2],p[3])
 
-        
-
-
-#WRITE RULES BELOW
-
-# Regla para definir las instrucciones permitidas por Setlan.
-def p_inst(p):
-    print('HOLAAAA')
-    if (len(p)==4):
-        if (p[1]=='|['):
-            p[0] = Block(None,p[0])
-
-    elif (len(p)==5):
-        if (p[1]=='|['):
-            p[0] = Block(p[2],p[3])
-
+        # Definicion de la regla para Delimitadores de la expresion.
+		# elif ((p[1] == '(') and (p[3] == ')')):
+		# 	p[0] = ParentisedExpression(p[1], p[2], p[3])
+		# elif ((p[1] == '[') and (p[3] == ']')):
+		# 	p[0] = BracketedExpression(p[1], p[2], p[3])
 
 
 # Regla para poder encontrar los errores sintácticos.
