400eeed74cbe91c3ac685bf16d073757dcc30b3a
Fernando Gonzalez <gonzalez0fer@gmail.com>
2019-10-27 22:49:31 -0400

    tree builded

--- Etapa2-Parser/g_AbsSyntaxTree.py
+++ Etapa2-Parser/g_AbsSyntaxTree.py
@@ -5,168 +5,34 @@
 # Kevin Mena 13-10869
 #######################################
 
-# Defino una variable global que fija un Tab de 2
-#espacios para la identacion del arbol.
-TAB = '  '
+# Defino una variable global que fija un Tab de
+#espacio para la identacion del arbol.
+TAB = '\t'
 
-# Clases para establecer la inicializacion del objeto que representara
-# al token ['__init__'] y un metodo de impresion para el arbol ['ToString'].
-class Block:
-    """ Definicion del objeto [Bloque], se inicializa con una expresion o
-    una declaracion."""
+class SyntaxLeaf:
 
-    def __init__(self,declaration,expression):
-        self.expression = expression
-        self.declaration = declaration
-
-    def toString(self, identation):
-        output = TAB*identation + 'Block\n'
-        if self.declaration != None:
-            output += self.declaration.toString(identation + 2)
-        if self.expression != None:
-            output += TAB*identation + 'kcolB\n'
-        return output
-
-class Declare:
-    """ Definicion del objeto [Declaracion], se inicializa con una lista 
-    de variables(Id's)"""
-    def __init__(self,_list):
-        self._list = _list
-
-    def toString(self,identation):
-        output = TAB*identation + 'Declare\n'
-        output += self._list.toString(identation + 2) 
-        output += TAB*identation + 'eralceD\n'
-
-        return output
-
-class ConditionalStatements:
-    """ Definicion del objeto [Declaracion Condicional], se inicializa con
-    una lista de tuplas de la forma [(conditional, instruction)] para hacer
-    la analogia del ELIF planteado en GuardedUSB"""
-    def __init__(self, _list):
-        self._list = _list
-
-    def toString(self, identation):
-        output = TAB*identation + 'IF\n'
-        for statement in self._list:
-            output += TAB*(identation+2) + 'Guard\n'
-            output += statement[0].toString(identation+4)
-        #debo definir de manera recursiva la impresion
-
-class BinaryOperator:
-    """ Definicion del objeto [Operador Binario], se inicializa con ambos
-    operandos mas el operador."""
-
-    def __init__(self, left_operand, operator, right_operand):
-        self.left_operand = left_operand
-        self.operator = operator
-        self.right_operand = right_operand
-
-    def toString(self,identation):
-        operators ={
-            '+'     : 'PLUS',
-            '-'     : 'MINUS',
-            '*'     : 'MULT',
-            '/'     : 'DIV',
-            '%'     : 'MODULE',
-           r'\/'    : 'OR',
-           r'/\\'   : 'AND',
-            '<'     : 'LESS',
-            '<='    : 'LESS_EQUAL',
-            '>'     : 'GREATER',
-            '>='    : 'GREATER_EQUAL',
-            '=='    : 'EQUALS',
-            '!='    : 'NOT_EQUAL',
-           r'\|\|'  : 'CONCAT',
-        }
-        output = TAB*identation + operators[self.operator] + ' ' + self.operator +'\n'
-        if isinstance(self.left_operand,OperandHandler):
-            if self.left_operand.data_type == 'id':
-                output += TAB*(identation + 2) + 'Ident:\n'
-                output += TAB*(identation + 4) + self.left_operand + '\n'
-            else:
-                output += self.left_operand.toString(identation +2)
-        else:
-            output += self.left_operand.toString(identation +2)
-        
-        if isinstance(self.right_operand,OperandHandler):
-            if self.right_operand.data_type == 'id':
-                output += TAB*(identation + 2) + 'Ident:\n'
-                output += TAB*(identation + 4) + self.left_operand + '\n'
-            else:
-                output += self.right_operand.toString(identation +2)
-        else:
-            output += self.right_operand.toString(identation +2)
-        return output
-
-class OperandHandler:
-    """Definicion para el objeto [Manejador de Operandos] el cual trata
-    las declaraciones los operandos derecho e izquierdo de los operadores 
-    binarios."""
-    def __init__(self,data_type,data_value):
-        self.data_type = data_type
-        self.data_value = data_value
-
-    def toString(self,identation):
-        if (self.data_type=='id'):
-            output = ' '*identation + 'Ident:' + '\n'
-            output += ' '*(identation + 2) + str(self.data_value) + '\n'
-        elif (self.data_type=='array'):
-            output = ' '*identation + self.data_type + '\n'
-            output += self.data_value.toString(identation + 2)
-        else:    
-            output = ' '*identation + self.data_type + '\n'
-            output += ' '*(identation + 2) + str(self.data_value) + '\n'
-        return output
-
-
-class DeclarationList:
-    def __init__(self, idList, _type, decList):
-        self.idList = idList
+    def __init__(self, _type, value = None, childs = None):
         self._type = _type
-        self.decList = decList
-
-    def toString(self,identation):
-        string = ' '*identation + self._type + '\n'
-        string += self.idList.toString(identation + 2)
-        if (self.decList!=None):
-            string += self.decList.toString(identation)
-        return string
+        self.value = value
+        self.visited = False
+        if childs:
+            self.childs = childs
+        else:
+            self.childs = []
 
 
-class IdList:
-    def __init__(self,idList,first_Id):
-        self.idList = idList
-        self.first_Id = first_Id
+def SyntaxTreePrinter(syntaxLeaf, identation):
+    if syntaxLeaf:
+        if(len(syntaxLeaf.childs)>0):
+            for leaf in syntaxLeaf.childs:
+                if leaf._type == 'Block':
+                    identation = identation + TAB
+                    print('\n', identation, leaf._type)
 
-    def toString(self,identation):
-        string = ''
-        if (self.idList!=None):
-            string += self.idList.toString(identation)
-        string += self.first_Id.toString(identation)
-        return string
+                elif (leaf._type == "Declare"):
+                    print('\n', identation, leaf._type)
+                    DeclareLeafPrinter(i, tabs)
 
-
-class InstructionList:
-    def __init__(self,first_instruction,second_instruction):
-        self.first_instruction = first_instruction
-        self.second_instruction = second_instruction
-
-    def toString(self,identation):
-        string = self.first_instruction.toString(identation)
-        if (self.second_instruction!=None):
-            string += self.second_instruction.toString(identation)
-        return string
-
-class Asignacion:
-    def __init__(self,var,valor):
-        self.var = var
-        self.valor = valor
-
-    def toString(self,identation):
-        string = TAB*identation + 'ASSIGN\n'
-        string += self.var.toString(identation + 2) 
-        string += TAB*(identation + 2) + 'value\n'
-        string += self.valor.toString(identation + 4) 
-        return string
+                elif (leaf._type == "Expression"):
+                    print("\n")
+                    imprimirExp(i, tabs)
--- Etapa2-Parser/g_parser.py
+++ Etapa2-Parser/g_parser.py
@@ -43,18 +43,34 @@ def p_program(p):
                 |   TkOBlock TkDeclare Declaration Content TkCBlock
                 |   TkOBlock TkDeclare Declaration TkCBlock
     '''
+    if (len(p)==4):
+        p[0] = SyntaxLeaf('Block', None, [p[2]])
+    elif (len(p)==6):
+        p[0] = SyntaxLeaf('Block', None, [p[4],p[5]])
+    else:
+        p[0] = SyntaxLeaf('Block', None, [p[4]])
 
+   
 
 def p_declaration(p):
     ''' Declaration :   Variables TkTwoPoints Datatype
                     |   Variables TkTwoPoints Datatype TkSemicolon Declaration
     '''
+    if (len(p)==4):
+        p[0] = SyntaxLeaf('Declare', None, [p[1],p[3]])
+    else:
+        p[0] = SyntaxLeaf('Declare', None, [p[1],p[3],p[5]])      
+
 
 
 def p_array(p):
     ''' Array   :   TkArray TkOBracket Terminal TkSoForth Terminal TkCBracket
                 |   TkArray TkOBracket AritmeticOperator TkSoForth AritmeticOperator TkCBracket
     '''
+    p[0] = SyntaxLeaf('Array', None, [p[3],p[5]])
+
+
+
 def p_terminal(p):
 	'''
 	Terminal :  TkId
@@ -65,6 +81,12 @@ def p_terminal(p):
               | TkQuote
 			  | TkOpenPar Terminal TkClosePar
 	'''
+	if (len(p) == 4):
+		p[0] = SyntaxLeaf('Terminal', None, [p[3]])
+	else:
+		p[0] = SyntaxLeaf('Terminal', p[1])
+
+
 
 def p_content(p):
 	''' Content :   Instruction
@@ -72,7 +94,14 @@ def p_content(p):
                 |   Block TkSemicolon Content
                 |   Block
 	'''
+	p[0] = SyntaxLeaf('Content', None, [p[1]])
+	if (len(p) == 2):
+		p[0] = SyntaxLeaf('Content', None, [p[1]])
+	else:
+		p[0] = SyntaxLeaf('Content', None, [p[1], p[3]])
     
+
+
 def p_instruction(p):
     ''' Instruction :   Conditional
                     |   Forloop
@@ -81,6 +110,9 @@ def p_instruction(p):
                     |   Input
                     |   Output
     '''
+    p[0] = SyntaxLeaf('Instruction', None, [p[1]])
+
+
 
 def p_conditional(p):
     ''' Conditional :   TkIf RelationalOperator TkArrow Content TkFi
@@ -88,6 +120,12 @@ def p_conditional(p):
                     |   TkIf RelationalOperator TkArrow Content Guard TkFi
                     |   TkIf BooleanOperator TkArrow Content Guard TkFi
     '''
+    if (len(p)==6):
+        p[0] = SyntaxLeaf('Conditional', None, [p[2],p[4]])
+    else:
+        p[0] = SyntaxLeaf('Conditional', None, [p[2],p[4],p[5]])
+
+
 
 def p_guard(p):
     ''' Guard   :   TkGuard RelationalOperator TkArrow Content
@@ -95,30 +133,59 @@ def p_guard(p):
                 |   TkGuard RelationalOperator TkArrow Content Guard
                 |   TkGuard BooleanOperator TkArrow Content Guard
     '''
+    if (len(p)==5):
+        p[0] = SyntaxLeaf('Guard', None, [p[2],p[4]])
+    else:
+        p[0] = SyntaxLeaf('Guard', None, [p[2],p[4],p[5]])
+
+
+
 
 def p_asign(p):
     ''' Asign    :   TkId TkAsig Expression
     '''
+    p[0] = SyntaxLeaf('Asign', p[1], [p[3]])
+
+
 
 def p_input(p):
-	''' Input   :   TkRead TkId
-	'''
+    ''' Input   :   TkRead TkId
+    '''
+    p[0] = SyntaxLeaf('Input', p[1], [p[2]])
+
+
 
 def p_output(p):
-	'''Output   :   TkPrint Expression
+    '''Output   :   TkPrint Expression
                 |   TkPrint Expression ConcatExpression
                 |   TkPrintln Expression
                 |   TkPrintln Expression ConcatExpression
-	'''
+    '''
+    if(len(p)==4):
+        p[0] = SyntaxLeaf('Output', None, [p[2],p[3]])
+    else:
+        p[0] = SyntaxLeaf('Output', None, [p[2]])
+
+
 
 def p_concatexp(p):
     ''' ConcatExpression : TkConcat Expression
                         |   TkConcat Expression ConcatExpression
     '''
+    if(len(p)==4):
+        p[0] = SyntaxLeaf('ConcatExpression', None, [p[2],p[3]])
+    else:
+        p[0] = SyntaxLeaf('ConcatExpression', None, [p[2]])
+
+
+
 def p_doloop(p):
     ''' Doloop :   TkDo RelationalOperator TkArrow Content TkOd
                 |   TkDo BooleanOperator TkArrow Content TkOd 
     '''
+    p[0] = SyntaxLeaf('Doloop', p[2], [p[4]])
+
+
 
 def p_forloop(p):
     ''' Forloop : TkFor TkId TkIn ArrayOperator TkTo ArrayOperator TkArrow Content TkRof
@@ -126,6 +193,9 @@ def p_forloop(p):
                 | TkFor TkId TkIn Terminal TkTo ArrayOperator TkArrow Content TkRof
                 | TkFor TkId TkIn Terminal TkTo Terminal TkArrow Content TkRof
     ''' 
+    p[0] = SyntaxLeaf('Forloop', p[2], [p[4],p[6],p[8]])
+
+
 
 def p_expression(p):
     ''' Expression  :   AritmeticOperator
@@ -136,33 +206,64 @@ def p_expression(p):
                     |   ArrayOperator
                     |   ArrayExpression
     '''
+    p[0] = SyntaxLeaf('Expression', None, [p[1]])
+
+
+
 def p_arrayexpres(p):
     ''' ArrayExpression :   TkId TkOpenPar Terminal TkTwoPoints Terminal TkClosePar
                         |   ArrayExpression TkOpenPar Terminal TkTwoPoints Terminal TkClosePar
                         |   TkId TkOBracket Terminal TkCBracket
                         |   ArrayExpression TkOBracket Terminal TkCBracket
     '''
+    if (len(p)==7):
+        if p[1] == 'TkId':
+            p[0] = SyntaxLeaf('ArrayExpression', p[1], [p[3],p[5]])
+        else:
+            p[0] = SyntaxLeaf('ArrayExpression', None, [p[1],p[3],p[5]])
+    else:
+        if p[1] == 'TkId':
+            p[0] = SyntaxLeaf('ArrayExpression', p[1], [p[3]])
+        else:
+            p[0] = SyntaxLeaf('ArrayExpression', None, [p[1],p[3]])
+
+
+
 
 def p_aritmoper(p):
-	''' AritmeticOperator : Expression TkMinus Expression
-                    |   Expression TkPlus Expression
-                    |   Expression TkDiv Expression
-                    |   Expression TkMult Expression
-                    |   Expression TkMod Expression
-                    |   TkOpenPar Expression TkMinus Expression TkClosePar
-                    |   TkOpenPar Expression TkPlus Expression TkClosePar
-                    |   TkOpenPar Expression TkDiv Expression TkClosePar
-                    |   TkOpenPar Expression TkMult Expression TkClosePar
-                    |   TkOpenPar Expression TkMod Expression TkClosePar
-                    |   TkMinus Expression %prec uminus
-                    |   TkOpenPar TkMinus Expression TkClosePar %prec uminus
+    ''' AritmeticOperator : Expression TkMinus Expression
+        |   Expression TkPlus Expression
+        |   Expression TkDiv Expression
+        |   Expression TkMult Expression
+        |   Expression TkMod Expression
+        |   TkOpenPar Expression TkMinus Expression TkClosePar
+        |   TkOpenPar Expression TkPlus Expression TkClosePar
+        |   TkOpenPar Expression TkDiv Expression TkClosePar
+        |   TkOpenPar Expression TkMult Expression TkClosePar
+        |   TkOpenPar Expression TkMod Expression TkClosePar
+        |   TkMinus Expression %prec uminus
+        |   TkOpenPar TkMinus Expression TkClosePar %prec uminus
     '''
+    if (len(p) == 6):
+        p[0] = SyntaxLeaf('AritmeticOperator', p[3], [p[2], p[4]])
+    elif (len(p) == 4):
+        p[0] = SyntaxLeaf('AritmeticOperator', p[2], [p[1], p[3]])
+    elif (len(p) == 3):
+        p[0] = SyntaxLeaf('UnaryAritmeticOperator', p[1], [p[2]])
+    else:
+        p[0] = SyntaxLeaf('UnaryAritmeticOperator', p[2], [p[3]])
+	
+		
 
 def p_Stroper(p):
-	''' StrOperator : TkId TkConcat TkId
-                | TkOpenPar TkId TkConcat TkId TkClosePar
-
+    ''' StrOperator : TkId TkConcat TkId
+        | TkOpenPar TkId TkConcat TkId TkClosePar
     '''
+    if (len(p)==6):
+        p[0] = SyntaxLeaf('StrOperator',[p[3],p[5]])
+    else:
+        p[0] = SyntaxLeaf('StrOperator',[p[1],p[3]])
+
 
 def p_arrayoper(p):
     ''' ArrayOperator   :   TkSize TkOpenPar TkId TkClosePar
@@ -174,6 +275,8 @@ def p_arrayoper(p):
                     | TkMin TkOpenPar Array TkClosePar
                     | TkAtoi TkOpenPar Array TkClosePar
     '''
+    p[0] = SyntaxLeaf('ArrayOperator',p[4])
+
 
 
 def p_opRel(p):
@@ -190,11 +293,23 @@ def p_opRel(p):
                 |   TkOpenPar Expression TkEqual Expression TkClosePar
                 |   TkOpenPar Expression TkNEqual Expression TkClosePar
 	'''
+	if (len(p) == 6):
+		p[0] = SyntaxLeaf('RelationalOperator', p[3], [p[2], p[4]])
+	else:
+		p[0] = SyntaxLeaf('RelationalOperator', p[2], [p[1], p[3]])
+
+
 
 def p_variables(p):
-	''' Variables : TkId TkComma Variables
+    ''' Variables : TkId TkComma Variables
                 | TkId 
-	'''
+    '''
+    if (len(p)==4):
+        p[0] = SyntaxLeaf('Variable', p[1], [p[3]])
+    else:
+        p[0] = SyntaxLeaf('Variable', p[1])    
+
+
 
 def p_boolop(p):
 	'''BooleanOperator : Expression TkAnd Expression
@@ -204,6 +319,18 @@ def p_boolop(p):
                 |   TkNot Expression
                 |   TkOpenPar TkNot Expression TkClosePar
 	'''
+	if (len(p) == 4):
+		p[0] = SyntaxLeaf('BooleanOperator', p[2], [p[1], p[3]])
+	elif (len(p) == 3):
+		p[0] = SyntaxLeaf('UnaryBooleanOperator', p[1], [p[2]])
+	elif (len(p) == 5):
+		p[0] = SyntaxLeaf('UnaryBooleanOperator', p[2], [p[3]])
+	elif(len(p) == 2):
+		p[0] = SyntaxLeaf('Terminal', p[1], [])
+	else:
+		p[0] = SyntaxLeaf('BooleanOperator', p[3], [p[2], p[4]])
+
+
 
 def p_datatype(p):
     ''' Datatype : TkInt
@@ -213,6 +340,11 @@ def p_datatype(p):
                 |   TkBool TkComma Datatype
                 |   Array TkComma Datatype
     '''
+    if (len(p)==4):
+        p[0] = SyntaxLeaf('Datatype', p[1], [p[3]])
+    else:
+        p[0] = SyntaxLeaf('Datatype', p[1])    
+
 
 
 def p_error(p):
