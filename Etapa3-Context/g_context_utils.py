e3b442a354b51d8c6051007a1c4f862ad217d2a6
Fernando Gonzalez <gonzalez0fer@gmail.com>
2019-11-24 20:53:43 -0400

    print expressions correctly

--- Etapa3-Context/g_context_utils.py
+++ Etapa3-Context/g_context_utils.py
@@ -14,23 +14,23 @@ def PrintSymbolTable(c_auxScopes, identation):
     sorttyp = sorted(types, key=len)
     longest_val = len(sortpre[-1])
     longest_type = len(sorttyp[-1])
-    margin_table = ' '*(((longest_val+longest_type)//2)+7)
+    margin_table = ' '*(((longest_val+longest_type)//2)+8)
 
     print(identation+color.BLUWHITE +margin_table+ "SYMBOLS TABLE"+margin_table+ color.END)
     scope = c_auxScopes[0]
     for i in scope:
         if len(scope[i].s_value) < longest_val:
             if len(scope[i].s_value) % 2 == 0:
-                print(identation+color.BLUE+'Variable '+color.END+' '*((longest_val-len(scope[i].s_value)))+scope[i].s_value+\
+                print(' '+identation+color.BLUE+'Variable '+color.END+' '*((longest_val-len(scope[i].s_value)))+scope[i].s_value+\
                     ' '+color.BLUE+'|'+color.END+ ' '+ color.BLUE+'Type '+color.END+scope[i].s_type)
             else:
-                print(identation+color.BLUE+'Variable '+color.END+' '*((longest_val-len(scope[i].s_value)))+scope[i].s_value+\
+                print(' '+identation+color.BLUE+'Variable '+color.END+' '*((longest_val-len(scope[i].s_value)))+scope[i].s_value+\
                     ' '+color.BLUE+'|'+color.END+ ' '+ color.BLUE+'Type '+color.END+scope[i].s_type)
 
         else:
             if (scope[i].is_array):
-                print(identation+color.BLUE+'Variable '+color.END+scope[i].s_value+' ' +color.BLUE+'|'+color.END+ ' '+ \
+                print(' '+identation+color.BLUE+'Variable '+color.END+scope[i].s_value+' ' +color.BLUE+'|'+color.END+ ' '+ \
                     color.BLUE+'Type '+color.END+scope[i].s_type + '[' + str(scope[i].array_indexes[0]) + '..' + str(scope[i].array_indexes[1]) + ']'+ ' int')
             else:
-                print(identation+color.BLUE+'Variable '+color.END+scope[i].s_value+' ' +color.BLUE+'|'+color.END+ ' '+ \
+                print(' '+identation+color.BLUE+'Variable '+color.END+scope[i].s_value+' ' +color.BLUE+'|'+color.END+ ' '+ \
                     color.BLUE+'Type '+color.END+scope[i].s_type)
